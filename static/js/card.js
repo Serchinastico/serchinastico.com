ecb7c9cf02f2f8f1670e31891543c1545184b8bb
Serchinastico <github@serchinastico.com>
2023-08-24 21:24:40 +0200

    Fix some of the animations in the card, still not good but I'm getting there

--- static/js/card.js
+++ static/js/card.js
@@ -1,42 +1,43 @@
 const ROTATION_X_FACTOR = 0.2;
 const SWAP_ROTATION_X = 90;
-const MAX_ROTATION_X = 600;
+const FIXED_THRESHOLD = 900;
 
+const clamp = (min, max, a) => Math.min(max, Math.max(min, a));
 const lerp = (x, y, a) => x * (1 - a) + y * a;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const card = document.getElementById("card");
+  const cardSection = document.querySelector("#card");
+  const card = document.querySelector(".card");
   const cardBoundingRect = card.getBoundingClientRect();
   let scrollRotationX = 0;
   let mouseRotationX = 0;
   let rotationY = 0;
 
   document.addEventListener("scroll", () => {
-    scrollRotationX =
-      ROTATION_X_FACTOR * Math.min(MAX_ROTATION_X, Math.max(0, window.scrollY));
+    scrollRotationX = clamp(0, 180, ROTATION_X_FACTOR * window.scrollY);
 
-    if (scrollRotationX > SWAP_ROTATION_X) {
-      console.log("SWAP{");
+    if (window.scrollY <= FIXED_THRESHOLD) {
+      cardSection.style.top = `${window.scrollY}px`;
     }
+
     card.style.transform = `rotateX(${
       scrollRotationX + mouseRotationX
     }deg) rotateY(${rotationY}deg)`;
   });
 
   card.addEventListener("mousemove", (event) => {
-    mouseRotationX = -lerp(
-      -30,
-      30,
-      (event.pageX - event.currentTarget.offsetLeft) / cardBoundingRect.width
-    );
-    rotationY = lerp(
-      -60,
-      180,
-      (event.pageY - event.currentTarget.offsetTop) / cardBoundingRect.height
-    );
-
-    card.style.transform = `rotateX(${
-      scrollRotationX + mouseRotationX
-    }deg) rotateY(${rotationY}deg)`;
+    // mouseRotationX = -lerp(
+    //   -30,
+    //   30,
+    //   (event.pageX - event.currentTarget.offsetLeft) / cardBoundingRect.width
+    // );
+    // rotationY = lerp(
+    //   -60,
+    //   180,
+    //   (event.pageY - event.currentTarget.offsetTop) / cardBoundingRect.height
+    // );
+    // card.style.transform = `rotateX(${
+    //   scrollRotationX + mouseRotationX
+    // }deg) rotateY(${rotationY}deg)`;
   });
 });
