cda48f0ec2d5d2a9e2663cd2d88fc89ad4734447
Serchinastico <github@serchinastico.com>
2023-09-05 12:15:17 +0200

    Add console message

---
+++ static/js/console.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(
+    "%c👋 Hey, Serchinastico here. \nIf you want to know how this webpage works, just visit my repository 👇",
+    `
+        padding: 8px;
+        background: #262931;
+        font-size: 1rem;
+        color: #FFF;
+        margin: 0;
+    `
+  );
+  console.log(
+    "%chttps://github.com/Serchinastico/serchinastico.com",
+    `
+        background: #262931;
+        border: 2px solid #FFBD3E;
+        color: #FFF;
+        font-size: 0.8rem;
+        margin: 12px;
+        padding: 16px;
+  `
+  );
+});
