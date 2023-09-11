7e7245be21b58b2540210721be9ba2c84109e67f
Serchinastico <github@serchinastico.com>
2023-09-11 19:46:01 +0200

    Add data section to include introduction and work sections

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # serchinastico.com
-My personal webpage
+
+My personal webpage.
+
+## Development
+
+This webpage is built with the classic HTML + CSS + JS stack. To separate data from the final HTML, I use node to render [EJS](https://github.com/mde/ejs) templates into one big HTML. Because this is a personal project that will most likely be maintained by me and only me, I'm taking the liberty of some "magic" (like loading scripts and style files automatically in the HTML if they are inside some specific directories).
+
+### Installing dependencies
+
+`yarn install`
+
+### Hot reload for development
+
+`yarn start:dev`
+
+### Compile EJS templates into HTML
+
+`yarn build`
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import ejs from "ejs";
+import YAML from "yaml";
 import fs from "fs";
+
+const toMap = (list, keyFn, contentFn) =>
+  list.reduce(function (acc, item) {
+    if (acc[keyFn(item)] === undefined) {
+      acc[keyFn(item)] = contentFn(item);
+    }
+    return acc;
+  }, {});
 
 const styleFileNames = fs.readdirSync("static/style");
 const styleFiles = styleFileNames.map((fileName) => `static/style/${fileName}`);
@@ -7,6 +16,20 @@ const styleFiles = styleFileNames.map((fileName) => `static/style/${fileName}`);
 const scriptFileNames = fs.readdirSync("static/js");
 const scriptFiles = scriptFileNames.map((fileName) => `static/js/${fileName}`);
 
+const dataFileNames = fs.readdirSync("data");
+const dataArray = dataFileNames
+  .map((fileName) => `data/${fileName}`)
+  .map((fileName) => ({
+    key: fileName.replace("data/", "").replace(".yaml", ""),
+    content: YAML.parse(fs.readFileSync(fileName, "utf-8")),
+  }));
+
+const data = toMap(
+  dataArray,
+  (item) => item.key,
+  (item) => item.content
+);
+
 const template = fs.readFileSync("template.ejs.html", "utf-8");
-const contents = ejs.render(template, { styleFiles, scriptFiles });
+const contents = ejs.render(template, { styleFiles, scriptFiles, data });
 fs.writeFileSync("index.html", contents);
