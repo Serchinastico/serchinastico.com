1076ce35e89c2477734ceeaac94395cab11f8264
Serchinastico <github@serchinastico.com>
2023-09-11 20:17:14 +0200

    Add hobbies data and fix footer CSS

--- static/js/hobbies.js
+++ static/js/hobbies.js
@@ -71,6 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const face = document.querySelector("#face");
   const selectedHobbyContents = document.querySelector("#selected-hobby");
   const woodworking = document.querySelector("#woodworking");
+  console.log(woodworking);
   const productDesign = document.querySelector("#product-design");
   const photography = document.querySelector("#photography");
   const musicComposition = document.querySelector("#music-composition");
