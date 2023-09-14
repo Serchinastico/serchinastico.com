3d9401f81095e648e275219286bd28c86887b1f7
Serchinastico <github@serchinastico.com>
2023-09-14 14:05:46 +0200

    Fix sticky positions in Safari

--- static/js/introduction.js
+++ static/js/introduction.js
@@ -1,7 +1,21 @@
 const MARGIN_IN_PX = 16;
 const BASE_MARGIN_BOTTOM_IN_PX = 128;
 
-document.addEventListener("DOMContentLoaded", () => {
+/**
+ * @param {DOMRect[]} bbs Array of bounding boxes of the nodes that are going to be measured
+ * @returns The height in pixels of all components if there were to be stacked vertically
+ */
+const getSectionsHeight = (bbs) =>
+  bbs.reduce((acc, bb) => acc + bb.height + MARGIN_IN_PX, 0);
+
+/**
+ * Here I'm using readystatechange instead of DOMContentLoaded because
+ * the former fires after a layout change causing the #fixed-intro element
+ * to move slightly up and messing with the top property of sticky nodes
+ */
+document.addEventListener("readystatechange", () => {
+  if (document.readyState !== "complete") return;
+
   const fixedIntro = document.querySelector("#fixed-intro");
   const sections = Array.from(document.querySelectorAll(".intro-sticky"));
   const introContainer = document.querySelector("#intro-container");
@@ -9,8 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const fixedIntroBB = fixedIntro.getBoundingClientRect();
   const sectionBBs = sections.map((section) => section.getBoundingClientRect());
 
-  let currentIntroTranslation = 0;
   const isSectionFixed = sections.map((_) => false);
+  let currentIntroTranslation = 0;
 
   /**
    * We calculate all the margin-bottoms by traversing items bottom-to-top
@@ -27,18 +41,16 @@ document.addEventListener("DOMContentLoaded", () => {
     nextMarginBottom += sectionBBs[nonReversedIndex].height + MARGIN_IN_PX;
   });
 
-  const sectionsHeight = sectionBBs.reduce(
-    (acc, bb) => acc + bb.height + MARGIN_IN_PX,
-    0
-  );
+  const sectionsHeight = getSectionsHeight(sectionBBs);
   const introFinalHeight = fixedIntroBB.height + sectionsHeight;
   fixedIntro.style.marginBottom = `${
     BASE_MARGIN_BOTTOM_IN_PX + introFinalHeight - fixedIntroBB.height
   }px`;
 
   /**
-   * Now that we have the intro sizes right, we send an event to the card
-   * component to leave the sticky position at the same time than the intro
+   * Now that we have the intro heights right, we send an event to the card
+   * component so that it leaves the screen at the same time than the intro
+   * by settings its bottom margin
    */
   document.dispatchEvent(
     new CustomEvent("intro-in-position", {
@@ -67,6 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
     nextTop += sectionBBs[index].height + MARGIN_IN_PX;
   });
 
+  /**
+   * We subscribe to the scroll event to translate the whole container
+   * when an item enters/exits the sticky state and keep everything centered in
+   * the screen
+   */
   let lastScroll = window.scrollY;
   document.addEventListener("scroll", () => {
     let isScrollingDown = window.scrollY - lastScroll > 0;
@@ -76,54 +93,29 @@ document.addEventListener("DOMContentLoaded", () => {
       section.getBoundingClientRect()
     );
 
-    if (isScrollingDown) {
-      const stickySectionIndex = sections.findIndex((_, index) => {
-        const scrollThreshold =
-          index === 0
-            ? // The first item has a threshold that depends on the fixed intro
-              fixedIntroTop + fixedIntroBB.height + MARGIN_IN_PX
-            : // The others can be calculated based on the previous sticky element
-              sectionsBBs[index - 1].bottom + MARGIN_IN_PX;
+    const stickySectionIndex = sections.findIndex((_, index) => {
+      const scrollThreshold =
+        index === 0
+          ? // The first item has a threshold that depends on the fixed intro
+            fixedIntroTop + fixedIntroBB.height + MARGIN_IN_PX
+          : // The others can be calculated based on the previous sticky element
+            sectionsBBs[index - 1].bottom + MARGIN_IN_PX;
 
-        return (
-          !isSectionFixed[index] && sectionsBBs[index].top <= scrollThreshold
-        );
-      });
+      return isScrollingDown
+        ? !isSectionFixed[index] && sectionsBBs[index].top <= scrollThreshold
+        : isSectionFixed[index] && sectionsBBs[index].top > scrollThreshold;
+    });
 
-      if (stickySectionIndex > -1) {
-        const newSectionsHeight = sectionBBs
-          .slice(0, stickySectionIndex + 1)
-          .reduce((acc, bb) => acc + bb.height + MARGIN_IN_PX, 0);
-        currentIntroTranslation = newSectionsHeight / 2;
-
-        introContainer.style.transform = `translateY(-${currentIntroTranslation}px)`;
-
-        isSectionFixed[stickySectionIndex] = true;
-      }
-    } else {
-      const stickySectionIndex = sections.findIndex((_, index) => {
-        const scrollThreshold =
-          index === 0
-            ? // The first item has a threshold that depends on the fixed intro
-              fixedIntroTop + fixedIntroBB.height + MARGIN_IN_PX
-            : // The others can be calculated based on the previous sticky element
-              sectionsBBs[index - 1].bottom + MARGIN_IN_PX;
-
-        return (
-          isSectionFixed[index] && sectionsBBs[index].top > scrollThreshold
-        );
-      });
-
-      if (stickySectionIndex > -1) {
-        const newSectionsHeight = sectionBBs
-          .slice(0, stickySectionIndex)
-          .reduce((acc, bb) => acc + bb.height + MARGIN_IN_PX, 0);
-        currentIntroTranslation = newSectionsHeight / 2;
-
-        introContainer.style.transform = `translateY(-${currentIntroTranslation}px)`;
-
-        isSectionFixed[stickySectionIndex] = false;
-      }
+    if (stickySectionIndex > -1) {
+      const newSectionsHeight = getSectionsHeight(
+        sectionBBs.slice(
+          0,
+          isScrollingDown ? stickySectionIndex + 1 : stickySectionIndex
+        )
+      );
+      currentIntroTranslation = newSectionsHeight / 2;
+      introContainer.style.transform = `translateY(-${currentIntroTranslation}px)`;
+      isSectionFixed[stickySectionIndex] = isScrollingDown;
     }
   });
 });
